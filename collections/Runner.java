bff9d47168493f836e28feb5881908a12fa34a63
JUMTBOX <yjey12@naver.com>
2024-08-24 22:04:44 +0900

    ...

--- collections/Runner.java
+++ collections/Runner.java
@@ -1,7 +1,9 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 public class Runner {
 
@@ -14,6 +16,11 @@ public class Runner {
     ArrayList<String> mutableList = new ArrayList<>(list);
     mutableList.add("fig");
     mutableList.add("grape");
+
+    LinkedList<String> mutableLinkedList = new LinkedList<>(list);
+    mutableLinkedList.add("apple2");
+    Vector<String> mutableVector = new Vector<>(list);
+    mutableVector.add("kiwi");
 
     mutableList.forEach(System.out::println); // Output: apple banana cherry date fig grape);
 
