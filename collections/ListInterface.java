c8b7d63d7427ae5ad0b594ab1f724186390b1ba9
JUMTBOX <yjey12@naver.com>
2024-08-25 17:21:52 +0900

    ...

--- collections/ListInterface.java
+++ collections/ListInterface.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ListInterface {
@@ -10,19 +11,36 @@ public class ListInterface {
     ArrayList<String> arrayList = new ArrayList<>(list);
 
     /* List는 중복된 요소를 가질 수 있다. */
-    arrayList.add(2,"date");
+    arrayList.add(2, "date");
 
     /* addAll 메서드 , arg1 - 요소들을 삽입할 인덱스, arg2 - 삽입할 요소들의 collection */
-    List<String> newlist = List.of("yak","zebra");
-    arrayList.addAll(2,newlist);
+    List<String> newlist = List.of("yak", "zebra");
+    arrayList.addAll(2, newlist);
 
     /* set 메서드 - 특정 인덱스의 요소만을 변경 */
-    arrayList.set(0,"dpple");
+    arrayList.set(0, "dpple");
 
     /* remove 메서드  -  특정 인덱스나 값을 받아 해당 요소를 삭제 */
     arrayList.remove(0);
     arrayList.remove("date");
 
-    arrayList.forEach(System.out::println);
+    /* 순회법 1 */
+    for(int i=0; i<arrayList.size(); i++) {
+      System.out.println(arrayList.get(i));
+    }
+
+    /* 순회 법 2 */
+    Iterator<String> iterator = arrayList.iterator();
+
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+    }
+
+    /* 순회법 3 */
+    for(String item : arrayList) {
+      System.out.println(item);
+    }
+
+    //    arrayList.forEach(System.out::println);
   }
 }
