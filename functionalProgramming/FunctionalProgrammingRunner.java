725d3b4cc3cf5d6a2c1fd99f1e3f8ebe9244ce5f
JUMTBOX <yjey12@naver.com>
2024-09-21 21:03:41 +0900

    ...

--- functionalProgramming/FunctionalProgrammingRunner.java
+++ functionalProgramming/FunctionalProgrammingRunner.java
@@ -6,8 +6,9 @@ public class FunctionalProgrammingRunner {
 
   public static void main(String[] args) {
     List<Integer> list = List.of(1,2,3,4,5);
-
-    printWithFP(list);
+    List<String> strList = List.of("Apple", "Bat","Cat","Dog");
+//    printWithFP(list);
+    printWithCondition(strList);
   }
 
   private static void printBasic(List<String> list) {
@@ -19,6 +20,14 @@ public class FunctionalProgrammingRunner {
   private static <T> void printWithFP(List<T> list) {
     list.stream().forEach(item -> {
       System.out.println(item);
+    });
+  }
+
+  private static void printWithCondition (List<String> list) {
+    list.stream().forEach(item ->{
+      if(item.endsWith("at")) {
+        System.out.println(item);
+      }
     });
   }
 }
