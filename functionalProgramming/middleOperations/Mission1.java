d3c9e878d04f54a6eb165aea7b8452281e6bb577
JUMTBOX <yjey12@naver.com>
2024-09-22 17:12:00 +0900

    ...

--- functionalProgramming/middleOperations/Mission1.java
+++ functionalProgramming/middleOperations/Mission1.java
@@ -1,13 +1,11 @@
 package functionalProgramming.middleOperations;
 
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class Mission1 {
 
   public static void main(String[] args) {
-    List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
-
-    String result = list.stream().map(e-> (int)Math.pow(e,2)).toList().toString();
+    String result = IntStream.range(1,10).map(e->(int)Math.pow(e,2)).boxed().toList().toString();
     System.out.println(result);
   }
 }
